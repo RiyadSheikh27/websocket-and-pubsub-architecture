08b11f3658eea6ec7f38282d2a2d1d314e6d9a33
RiyadSheikh27 <riyad.cse27@gmail.com>
2026-01-28 15:36:23 +0600

    primary setup of project

--- django_channels/apps.py
+++ django_channels/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AdvanceDjangoChannelConfig(AppConfig):
+class DjangoChannelsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'advance_django_channel'
+    name = 'django_channels'
